7b7fc1c58f882e7d8633f72d5fe042367e130f93
DanielleInkster <danielle.inkster@gmail.com>
2020-02-13 01:20:40 +0000

    removed echos'

--- attempt-2.php
+++ attempt-2.php
@@ -5,7 +5,6 @@ $sensitive_data = ['CardCVV', 'cardCVV', 'CVVCVCSecurity', 'CardNumber',
 '07_Account_Card_Number', '07_Account_Expiry', '17_CVV'];
 
 function redactInfo($string, $sensitive_data){;
-    
     if (is_object(json_decode($string))){  
         jsonString($sensitive_data, $string);
     } else if (substr($string, 0, 7) == "Request"){
@@ -14,7 +13,7 @@ function redactInfo($string, $sensitive_data){;
         xmlString($sensitive_data, $string);
     } else {
         arrayString($sensitive_data, $string);
- }
+    }
 }
 
 // identifies and redacts specified values
@@ -32,7 +31,6 @@ function jsonString($sensitive_data, $string){
     $array = json_decode($string, TRUE);
         $array = searchInfo($sensitive_data, $array);
         $string = json_encode($array);
-        echo strval($string);
         return strval($string);
 }
 //manipulates data in a xml string - return is not yet complete
@@ -40,24 +38,21 @@ function xmlString($sensitive_data, $string){
     $json= json_encode(simplexml_load_string($string));
         $array = json_decode($json, TRUE);
         $array = $array["NewOrder"];
-
         $array =searchInfo($sensitive_data, $string);
-         $array = array_flip($array);
-         $xml = new SimpleXMLElement('<NewOrder/>');
-         array_walk_recursive($array, array ($xml, 'addChild'));
-         $string =  $xml->asXML();
-         echo strval($string);
-         return strval($string);
+        $array = array_flip($array);
+        $xml = new SimpleXMLElement('<Request/>');
+        array_walk_recursive($array, array ($xml, 'addChild'));
+        $string =  $xml->asXML();
+        return strval($string);
 }
 //manipulates data in a query string
 function queryString($sensitive_data, $string){
         parse_str($string, $array);
         $array = searchInfo($sensitive_data, $array);
         $string = urldecode(http_build_query($array));
-        echo strval($string);
         return strval($string);
 }
-//manipulates data in an array string - return is not yet complete
+//manipulates data in an array string - too bulky and return is not yet complete
 function arrayString($sensitive_data, $string){
     $string = str_replace("\n", "&", $string);
         $string = str_replace(" => ", "=", $string);
@@ -65,8 +60,25 @@ function arrayString($sensitive_data, $string){
         $string = str_replace("]", "", $string);
         parse_str($string, $array);
         $array = searchInfo($sensitive_data, $array); 
-        print_r($array);
         return $array;
 }
 
 ;
+
+$string_1 = "[orderId] => 212939129
+[orderNumber] => INV10001
+[salesTax] => 1.00
+[amount] => 21.00
+[terminal] => 5
+[currency] => 1
+[type] => purchase
+[avsStreet] => 123 Road
+[avsZip] => A1A 2B2
+[customerCode] => CST1001
+[cardId] => 18951828182
+[cardHolderName] => John Smith
+[cardNumber] => 5454545454545454
+[cardExpiry] => 1025
+[cardCVV] => 100";
+
+redactInfo($string_1,$sensitive_data);
